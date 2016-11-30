bc52530f1352290567bbf0b31aadbd75b098b3d3
Eric C. Anderson <github@ericanderson.com>
2016-11-30 15:08:05 -0600

    Initial checking of actual data.

---
+++ at.R
@@ -0,0 +1,7 @@
+# library(gdata)	#load gdata package
+getwd()
+setwd("C:/users/eric.c.anderson/Source/Repos/analyzethis/")
+getwd()
+mydata = read.csv("Analyze+This+Data+and+Metadata.csv") # read csv file
+head(mydata)
+names(mydata)
